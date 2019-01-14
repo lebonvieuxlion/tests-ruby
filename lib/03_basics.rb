5c76e291dccfc2ea8e801d1ff14b22be884968d3
lebonvieuxlion <debaugelion@gmail.com>
2019-01-14 14:50:15 +0100

    Exercice 03 réalisé

--- lib/03_basics.rb
+++ lib/03_basics.rb
@@ -13,4 +13,33 @@ def who_is_bigger(a, b, c)
 	end
 end
 
-def reverse_upcase_noLTA
+
+
+def reverse_upcase_noLTA(a)
+
+	a = a.reverse 						#reverse les lettres
+	a = a.upcase  						#les mets en majuscule
+	a.gsub(/[ATL]/, "")					#enlève les A, T et L 
+
+end
+
+
+
+def array_42(a=[])
+
+	a.include?(42)				#cherche dans chaque élément si il contient l'élément 42
+
+end
+
+
+
+def magic_array(a=[])
+	a = a.flatten					#regroupe tous les array dans un unique array (création d'un nouvel array)
+	a = a.sort						#classe tous les éléments du array du plus petit au plus grand
+	a = a.collect{|value|value*2}		#multiplie tous les éléments d'un array par 2
+	a = a.reject {|value| value % 3 == 0}
+	a = a.uniq
+	a = a.sort
+end
+
+puts magic_array([1, 2, 3, 4, 5, 6])
